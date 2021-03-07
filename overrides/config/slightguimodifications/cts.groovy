2e9a76758a4074a4e4029bf27c761f12719fc813
Madis Otenurm <Madis0@users.noreply.github.com>
2021-03-07 10:29:06 +0200

    1.7.1 CF

---
+++ overrides/config/slightguimodifications/cts.groovy
@@ -0,0 +1,15 @@
+mainMenu {
+    enabled = true
+    label {
+        position {
+            x { it - 2 }
+            y { it - 20 }
+        }
+        text = literal("Fabulously Optimized 1.7.1")
+        align = "right"
+        color = 0xFFFFFF
+        hoveredColor = 0x55FFFF
+        shadow = true
+        onClicked = url("https://www.curseforge.com/minecraft/modpacks/fabulously-optimized")
+    }
+}
