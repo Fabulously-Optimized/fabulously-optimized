c355745745978bc0a0e6be4e30d612fda22b2e21
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-04-24 22:41:10 +0300

    Add pre-launch.sh and post-exit.sh (#290)
    * Mod disabler (Linux and MacOS propably)
    
    * Update pre-launch.sh
    
    * Small spelling mistake 😅
    
    Co-authored-by: Madis Otenurm <Madis0@users.noreply.github.com>
    
    * `$mod3.jar\ ` was placed 2 times
    
    * MC version from the instance's `mmc-pack.json`
    
    * Update pre-launch.sh
    
    * Spam fix
    
    * Spam fix
    
    Co-authored-by: Madis Otenurm <Madis0@users.noreply.github.com>

---
+++ Packwiz/pre-launch.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Should be ran from the pre-launch command in instance's settings.
+# Edits by RaptaG (https://github.com/RaptaG)
+
+# Select the mods you wish to disable:
+mod0=
+mod1=
+mod2=
+mod3=
+mod4=
+mod5=
+
+# Upgrading Fabulously Optimized
+echo Checking for FO upgrades...
+cd ..
+mcver="$(jq -r '.components[]|select(.cachedName=="Minecraft")|.version' mmc-pack.json)"
+if [ -d .minecraft ]; then
+   cd .minecraft
+else
+   cd minecraft
+fi
+"$INST_JAVA" -jar packwiz-installer-bootstrap.jar https://raw.githubusercontent.com/Fabulously-Optimized/fabulously-optimized/main/Packwiz/$mcver/pack.toml
+
+# Upgrading the checksums
+checksums="$(md5sum packwiz.json | cut -d' ' -f1)"
+echo "$checksums  packwiz.json" | md5sum -c - || exit 1
+
+# Disabling the mods
+echo Disabling mods...
+cd mods
+for mod in\
+	$mod1.jar\
+	$mod2.jar\
+	$mod3.jar\
+	$mod4.jar\
+	$mod5.jar\
+	$mod0.jar
+do
+        mv $mod .$(basename $mod .jar).jar.disabled
+        echo $mod disabled successfully!
+done
