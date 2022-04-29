757d90039349190aab9172b8e772a1032e384083
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-04-29 22:11:20 +0100

    cleanup

--- Packwiz/pre-launch.ps1
+++ Packwiz/pre-launch.ps1
@@ -1,14 +1,13 @@
-﻿# By Ultrasonic#7662
+# By Ultrasonic1209
 
 $mods = @(
-    'farsight.fabric-1.18-1.8.jar'
+    'ModYouDislike.jar'
+    'AnotherMod.jar'
 )
 
 # Upgrading Fabulously Optimized
 Write-Output "Checking for FO updates..."
 Set-Location ..
-
-#Set-Location "C:\Users\sonic\AppData\Roaming\PolyMC\instances\afd8656e-675c-464a-8e56-0d2676af0ad7\" #TEMP
 
 $json = Get-Content "mmc-pack.json" | ConvertFrom-Json
 
@@ -20,8 +19,6 @@ if (Test-Path -Path ".\.minecraft") {
 } else {
     Set-Location ".\minecraft"
 }
-
-#$INST_JAVA = "C:/Program Files/Eclipse Adoptium/jdk-17.0.1.12-hotspot/bin/javaw.exe" #TEMP
 
 $processOptions = @{
     FilePath = $Env:INST_JAVA
