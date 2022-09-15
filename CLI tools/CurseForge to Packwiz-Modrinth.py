2b74f9d3cf40c4177031b1418c232a2117d58bd0
RozeFound <rozefound@gmail.com>
2022-09-15 19:01:15 +0700

    Use mmc-export for packwiz pack

--- CLI tools/CurseForge to Packwiz-Modrinth.py
+++ CLI tools/CurseForge to Packwiz-Modrinth.py
@@ -1,5 +1,6 @@
 import os
 from zipfile import ZipFile
+from shutil import unpack_archive
 from pathlib import Path
 import toml # pip install toml
 
@@ -20,6 +21,7 @@ hydrogen = False
 modrinth_overrides = True
 mmc_export_modrinth_export = True
 packwiz_modrinth_export = False
+mmc_export_packwiz_export = True
 
 def extract_file(from_zip, from_file, to_path, from_desc, to_desc):
     with ZipFile(from_zip, 'r') as zip:
@@ -37,9 +39,11 @@ if refresh_only == False:
 os.chdir(packwiz_path)
 if refresh_only == False:
     cf_zip_path = input("Please drag the CurseForge zip file here: ")[3:][:-1] # Because dragging the file adds "& " and double quotes
-    
-    # Update pack.toml first
     pack_version = "-".join(str(Path(cf_zip_path).with_suffix("")).split("-")[1:])
+
+if mmc_export_packwiz_export == False and refresh_only == False:    
+
+    # Update pack.toml first
     with open(packwiz_manifest, "r") as f:
         pack_toml = toml.load(f)
     pack_toml["version"] = pack_version
@@ -54,13 +58,31 @@ if refresh_only == False:
     if modrinth_overrides:
         os.system(packwiz_exe_path + " remove entityculling")
         os.system(packwiz_exe_path + " mr install entityculling")
-os.system(packwiz_exe_path + " refresh")
+        
+else: os.system(packwiz_exe_path + " refresh")
 
 # Copy fresh manifest/modlist to git
 if is_legacy == False and refresh_only == False:
     extract_file(cf_zip_path, "manifest.json", git_path + "CurseForge", "CurseForge manifest.json", "Git")
     extract_file(cf_zip_path, "modlist.html", git_path + "CurseForge", "CurseForge modlist.html", "Git")
 
+# Export packwiz pack and manifest via mmc-export method
+
+if mmc_export_packwiz_export and not refresh_only:
+
+    mmc_zip_root = str(Path(cf_zip_path).parents[0])
+    mmc_zip_path = mmc_zip_root + "\\Fabulously Optimized " + pack_version + ".zip"
+    packwiz_config = git_path + "Packwiz\\mmc-export.toml"
+
+    cmd = f'mmc-export -i "{mmc_zip_path}" -f packwiz --modrinth-search loose -o "{mmc_zip_root}" -c "{packwiz_config}" -v {pack_version} --provider-priority Modrinth CurseForge Other --scheme ' + '"mmc_export_packwiz_output"'
+    os.system(cmd)
+
+    packwiz_zip_path = Path(mmc_zip_root) / "mmc_export_packwiz_output.zip" 
+    packwiz_out_path = Path(git_path) / "Packwiz" / minecraft_version
+    packwiz_out_path.mkdir(parents=True, exist_ok=True)
+    unpack_archive(packwiz_zip_path, packwiz_out_path)
+
+    os.remove(packwiz_zip_path)
 
 # Export Modrinth pack and manifest via mmc-export method
 if mmc_export_modrinth_export:
