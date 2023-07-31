de5cfa9dc6706ac4d6b192829c1a87ec92f58d0d
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-07-31 15:23:18 +0500

    Refactor 'CurseForge to MultiMC-Git.py'
    * Replace os.path with pathlib
    * Use f-strings instead of +
    * Format code according to PEP8
    * Add type annotations

--- CLI tools/CurseForge to MultiMC-Git.py
+++ CLI tools/CurseForge to MultiMC-Git.py
@@ -1,55 +1,57 @@
-import os, shutil
+import shutil
+from pathlib import Path
 
-user_path = os.path.expanduser("~")
-cf_path = user_path + "/curseforge/minecraft/Instances/Fabulously Optimized/"
-mmc_path = user_path + "/Documents/MultiMC/instances/Fabulously Optimized/minecraft/"
-git_path = user_path + "/Documents/GitHub/fabulously-optimized/"
+
+cf_path = Path(Path.home(), "/curseforge/minecraft/Instances/Fabulously Optimized/")
+mmc_path = Path(Path.home(), "/Documents/MultiMC/instances/Fabulously Optimized/minecraft/")
+git_path = Path(Path.home(), "/Documents/GitHub/fabulously-optimized/")
 minecraft_version = "1.20.1"
-packwiz_path = git_path + "Packwiz/" + minecraft_version
+packwiz_path = Path(git_path, "Packwiz", minecraft_version)
 
-# Functions
 
-def remove_dir(path, description):
-    if os.path.isdir(path):
+def remove_dir(path: Path, description: str) -> None:
+    if path.is_dir():
         shutil.rmtree(path)
-        print("Deleted " + description)
+        print(f"Deleted {description}")
     else:
-        print("Skipped " + description + " deletion, didn't exist")
+        print(f"Skipped {description} deletion, didn't exist")
 
-def remove_file(path, description):
-    if os.path.isfile(path):
-        os.remove(path)
-        print("Deleted " + description)
+
+def remove_file(path: Path, description: str) -> None:
+    if path.is_file():
+        path.unlink()
+        print(f"Deleted {description}")
     else:
-        print("Skipped " + description + " deletion, didn't exist")
+        print(f"Skipped {description} deletion, didn't exist")
 
-def copy_dir(from_path, to_path, from_desc, to_desc):
-    if os.path.isdir(from_path):
+
+def copy_dir(from_path: Path, to_path: Path, from_desc: str, to_desc: str) -> None:
+    if from_path.is_dir():
         shutil.copytree(from_path, to_path, dirs_exist_ok=True)
-        print("Copied " + from_desc + " to " + to_desc)
+        print(f"Copied {from_desc} to {to_desc}")
     else:
-        print("Skipped " + from_desc + " copying to " + to_desc + ", didn't exist")
+        print(f"Skipped {from_desc} copying to {to_desc}, didn't exist")
 
-def copy_file(from_path, to_path, from_desc, to_desc):
-    if os.path.isfile(from_path):
+
+def copy_file(from_path: Path, to_path: Path, from_desc: str, to_desc: str) -> None:
+    if from_path.is_file():
         shutil.copy2(from_path, to_path)
-        print("Copied " + from_desc + " to " + to_desc)
+        print(f"Copied {from_desc} to {to_desc}")
     else:
-        print("Skipped " + from_desc + " copying to " + to_desc + ", didn't exist")
+        print(f"Skipped {from_desc} copying to {to_desc}, didn't exist")
+
 
 # CurseForge to MultiMC
+remove_dir(Path(mmc_path, "mods"), "MultiMC mods")
+remove_dir(Path(mmc_path, "config"), "MultiMC configs")
+remove_dir(Path(mmc_path, "resourcepacks"), "MultiMC resourcepacks")
+remove_file(Path(mmc_path, "options.txt"), "MultiMC options.txt")
+copy_dir(Path(cf_path, "mods"), Path(mmc_path, "mods"), "CurseForge mods", "MultiMC")
+copy_dir(Path(cf_path, "config"), Path(mmc_path, "config"), "CurseForge configs", "MultiMC")
+copy_dir(Path(cf_path, "resourcepacks"), Path(mmc_path, "resourcepacks"), "CurseForge resource packs", "MultiMC")
 
-remove_dir(mmc_path + "config/", "MultiMC configs")
-remove_file(mmc_path + "options.txt", "MultiMC options.txt")
-remove_dir(mmc_path + "mods/", "MultiMC mods")
-remove_dir(mmc_path + "resourcepacks/", "MultiMC resourcepacks")
-copy_dir(cf_path + "config/", mmc_path + "config/", "CurseForge configs", "MultiMC")
-copy_dir(cf_path + "mods/", mmc_path + "mods/", "CurseForge mods", "MultiMC")
-copy_dir(cf_path + "resourcepacks/", mmc_path + "resourcepacks/", "CurseForge resource packs", "MultiMC")
-
-# CurseForge to Git 
-
+# CurseForge to Git
 # Manifest and json copying moved to "CurseForge to Packwiz.py" to make sure they are always newest
-remove_dir(packwiz_path + "/config/", "Packwiz configs in Git")
-copy_dir(cf_path + "config/", packwiz_path + "/config/", "CurseForge configs", "Git (Packwiz)")
-copy_dir(cf_path + "resourcepacks/", packwiz_path + "/resourcepacks/", "CurseForge resource packs", "Git (Packwiz)")
+remove_dir(Path(packwiz_path, "config"), "Packwiz configs in Git")
+copy_dir(Path(cf_path, "config"), Path(packwiz_path, "config"), "CurseForge configs", "Git (Packwiz)")
+copy_dir(Path(cf_path, "resourcepacks"), Path(packwiz_path, "resourcepacks"), "CurseForge resource packs", "Git (Packwiz)")
